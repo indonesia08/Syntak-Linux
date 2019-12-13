37de9d4ed9eb78b894b78c18b41036ed434b749b
Yusuf <40708511+indonesia08@users.noreply.github.com>
2019-12-13 22:01:37 +0700

    Create model.java

---
+++ model.java
@@ -0,0 +1,7 @@
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("#");
+        model.addColumn("NO REKENING");
+        model.addColumn("NAMA PEMILIK");
+        model.addColumn("JENIS REKENING");
+        model.addColumn("ID TELEGRAM");
+        model.addColumn("SALDO"); 
